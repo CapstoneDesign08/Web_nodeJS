25b20c7bef0dde354094868b03836b236e7955ce
hhhhm8826 <hhhhm8826@gmail.com>
2017-02-06 17:14:09 +0900

    Unity start

---
+++ Web_NodeJS/Unity_final/routes/index.js
@@ -0,0 +1,21 @@
+﻿var express = require('express');
+var process = require('child_process');
+
+var router = express.Router();
+
+router.get('/', function (req, res) {
+    var id = 1;
+        process.exec("C:\\Progra~1\\Unity\\Editor\\Unity.exe -projectPath \"C:\\Users\\hong\\Desktop\\DllTest\" -quit -batchmode -executeMethod WebGLBuilder.build", function (err, stdout, stderr) {
+            if (err) {
+                console.error(err);
+                return;
+            }
+
+            //print result on console
+            console.log(stdout);
+            console.log("fin");
+        });
+    //console.log("output");
+});
+
+module.exports = router;
