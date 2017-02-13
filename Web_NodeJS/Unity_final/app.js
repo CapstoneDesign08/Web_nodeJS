e6c512e2a5c21a48596efc37356b36498c169f5c
hhhhm8826 <hhhhm8826@gmail.com>
2017-02-13 16:42:45 +0900

    1Cycle Win+Web (hong)

--- Web_NodeJS/Unity_final/app.js
+++ Web_NodeJS/Unity_final/app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var fs = require('fs');
 var process = require('child_process');
+var engines = require('consolidate');
 
 var routes = require('./routes/index');
 var upload = require('./routes/upload');
@@ -13,7 +14,9 @@ var webgl = require('./routes/webgl');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.engine('jade', engines.jade);
+app.engine('html', engines.ejs);
+//app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
@@ -28,12 +31,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/upload', upload);
-app.use('/webgl', webgl);
+//app.use('/webgl', webgl);
+
+app.use('/webgl', express.static(path.join('/Users/hong/Desktop/TankTest_Grade/WebGL-Dist')));
 
 app.post('/upload', function (req, res, next) {
     var id = req.body.id;
     var play = req.body.play;
-    fs.writeFileSync('C:\\Users\\hong\\Desktop\\visualstudioTest\\MyAssembly.cs', req.body.source, 'utf8');
+    fs.writeFileSync('C:\\Users\\hong\\Desktop\\TankTest_Grade\\Assets\\Scripts\\Player\\Player.cs', req.body.source, 'utf8');
     fs.writeFileSync('C:\\Users\\hong\\Desktop\\TankTest_Grade\\Assets\\Scripts\\Player\\Player_ID.cs', "using System.Collections;using System.Collections.Generic;using UnityEngine;public class Player_ID : MonoBehaviour{ public static int player_id = "+id+";}", 'utf8');
     console.log('FileSync completed');
     if (play == 1) {         //score test
@@ -45,30 +50,30 @@ app.post('/upload', function (req, res, next) {
         });*/
         //unity start
         process.exec("C:\\Progra~1\\Unity\\Editor\\Unity.exe -projectPath \"C:\\Users\\hong\\Desktop\\TankTest_Grade\" -batchmode -quit -buildWindows64Player C:\\Users\\hong\\Desktop\\TankTest_Grade\\test.exe", function (err, stdout, stderr) {
-            errchk(err);
-            console.log("run complete user - " + id);
-            process.exec("start C:\\Users\\hong\\Desktop\\TankTest_Grade\\test.exe -batchmode -nographics", function (err, stdout, stderr) {
-                errchk(err);
-                console.log("Unity play fin");
-            });
+            if (err != null)
+                console.log(err);
+            else {
+                console.log("run complete user - " + id);
+                process.exec("start C:\\Users\\hong\\Desktop\\TankTest_Grade\\test.exe -batchmode -nographics", function (err, stdout, stderr) {
+                    console.log(err);
+                    console.log("Unity play fin");
+                });
+            }
         });
     }
     else if (play == 2) {     //web view test
-        //make dll
+        console.log("build complete user - " + id);
         //unity start
         process.exec("C:\\Progra~1\\Unity\\Editor\\Unity.exe -projectPath \"C:\\Users\\hong\\Desktop\\TankTest_Grade\" -batchmode -quit -executeMethod WebGLBuilder.build", function (err, stdout, stderr) {
-            errchk(err);
-
-            //print result on console
-            console.log("build complete user - " + id);
+            if (err != null)
+                console.log(err);
+            else {
+                //print result on console
+                console.log("build complete user - " + id);
+            }
         });
     }
 });
-
-var errchk = function errchk(err) {
-    if (err != null)
-        console.log(err);
-}
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- Web_NodeJS/Unity_final/routes/webgl.js
+++ Web_NodeJS/Unity_final/routes/webgl.js
@@ -1,10 +1,11 @@
 ﻿var express = require('express');
 var router = express.Router();
 
-
 /* GET home page. */
 router.get('/', function (req, res) {
-    //res.render('/Users/hong/Desktop/TankTest_Grade/WebGL-Dist/index', { title: 'WebGL' });
+    res.render('/Users/hong/Desktop/TankTest_Grade/WebGL-Dist/index.html', { title: 'WebGL' });
+    //res.render('index.html', { title: 'WebGL' });
+    
 
 });
 
