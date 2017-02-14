98bd4e5a94aec9bed3341f10c498c75716918d44
hhhhm8826 <hhhhm8826@gmail.com>
2017-02-14 17:41:54 +0900

    source refresh2 (hong)

--- Web_NodeJS/Unity_final/app.js
+++ Web_NodeJS/Unity_final/app.js
@@ -1,4 +1,5 @@
-﻿var express = require('express'); 
+﻿//module
+var express = require('express'); 
 var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
@@ -7,16 +8,15 @@ var fs = require('fs');
 var process = require('child_process');
 var engines = require('consolidate');
 
+//import route file
 var routes = require('./routes/index');
 var upload = require('./routes/upload');
 
 var app = express();
 
-
-// view engine setup
+// view engine setup - jade+ejs
 app.engine('jade', engines.jade);
 app.engine('html', engines.ejs);
-//app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
@@ -27,40 +27,53 @@ app.use(cookieParser());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
+//path  (path.join, fs.writeFileSync, process.exec)
+var webgl_path = "/Users/hong/Desktop/TankTest_WebGL/WebGL-Dist";
+
+var grade_player = "C:\\Users\\hong\\Desktop\\TankTest_Grade\\Assets\\Scripts\\Player\\Player.cs";
+var grade_player_id = "C:\\Users\\hong\\Desktop\\TankTest_Grade\\Assets\\Scripts\\Player\\Player_ID.cs";
+var webgl_player = "C:\\Users\\hong\\Desktop\\TankTest_WebGL\\Assets\\Scripts\\Player\\Player.cs";
+var webgl_player_id = "C:\\Users\\hong\\Desktop\\TankTest_WebGL\\Assets\\Scripts\\Player\\Player_ID.cs";
+
+var grade_build = "C:\\Progra~1\\Unity\\Editor\\Unity.exe -projectPath \"C:\\Users\\hong\\Desktop\\TankTest_Grade\" -batchmode -quit -buildWindows64Player C:\\Users\\hong\\Desktop\\TankTest_Grade\\test.exe";
+var grade_run = "start C:\\Users\\hong\\Desktop\\TankTest_Grade\\test.exe -batchmode -nographics";
+var webgl_build = "C:\\Progra~1\\Unity\\Editor\\Unity.exe -projectPath \"C:\\Users\\hong\\Desktop\\TankTest_WebGL\" -batchmode -quit -executeMethod WebGLBuilder.build";
+
+//route
 app.use('/', routes);
 app.use('/upload', upload);
-//app.use('/webgl', webgl);
-
-app.use('/webgl', express.static(path.join('/Users/hong/Desktop/TankTest_WebGL/WebGL-Dist')));
+app.use('/webgl', express.static(path.join(webgl_path)));
 
 app.post('/upload', function (req, res, next) {
     var id = req.body.id;
     var play = req.body.play;
     res.send('good request');
     if (play == 1) {         //score test
+        //write file
         var pass_id = "using System.Collections;using System.Collections.Generic;using UnityEngine;public class Player_ID : MonoBehaviour{ public static int player_id = " + id + ";}"
-        fs.writeFileSync('C:\\Users\\hong\\Desktop\\TankTest_Grade\\Assets\\Scripts\\Player\\Player.cs', req.body.source, 'utf8');
-        fs.writeFileSync('C:\\Users\\hong\\Desktop\\TankTest_Grade\\Assets\\Scripts\\Player\\Player_ID.cs', pass_id, 'utf8');
-        fs.writeFileSync('C:\\Users\\hong\\Desktop\\TankTest_WebGL\\Assets\\Scripts\\Player\\Player.cs', req.body.source, 'utf8');
-        fs.writeFileSync('C:\\Users\\hong\\Desktop\\TankTest_WebGL\\Assets\\Scripts\\Player\\Player_ID.cs', pass_id, 'utf8');
+        fs.writeFileSync(grade_player, req.body.source, 'utf8');
+        fs.writeFileSync(grade_player_id, pass_id, 'utf8');
+        fs.writeFileSync(webgl_player, req.body.source, 'utf8');
+        fs.writeFileSync(webgl_player_id , pass_id, 'utf8');
         console.log('FileSync completed');
-        //unity start
-        process.exec("C:\\Progra~1\\Unity\\Editor\\Unity.exe -projectPath \"C:\\Users\\hong\\Desktop\\TankTest_Grade\" -batchmode -quit -buildWindows64Player C:\\Users\\hong\\Desktop\\TankTest_Grade\\test.exe", function (err, stdout, stderr) {
+
+        //unity exe build
+        process.exec(grade_build, function (err, stdout, stderr) {
             if (err != null)
                 console.log(err);
-            else{
+            else{   //unity start
                 console.log("build complete user - " + id);
-                process.exec("start C:\\Users\\hong\\Desktop\\TankTest_Grade\\test.exe -batchmode -nographics", function (err, stdout, stderr) {
+                process.exec(grade_run, function (err, stdout, stderr) {
                     console.log(err);
                     console.log("Unity play fin");
                 });
             }
         });
     }
-    else if (play == 2) {     //web view test
+    else if (play == 2) {     //web view
         console.log("Web build start");
-        //unity start
-        process.exec("C:\\Progra~1\\Unity\\Editor\\Unity.exe -projectPath \"C:\\Users\\hong\\Desktop\\TankTest_WebGL\" -batchmode -quit -executeMethod WebGLBuilder.build", function (err, stdout, stderr) {
+        //unity web build
+        process.exec(webgl_build, function (err, stdout, stderr) {
             if (err != null)
                 console.log(err);
             else {
@@ -71,16 +84,15 @@ app.post('/upload', function (req, res, next) {
     }
 });
 
+// error handlers
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
-// error handlers
 
-// development error handler
-// will print stacktrace
+// development error handler, will print stacktrace
 if (app.get('env') === 'development') {
     app.use(function (err, req, res, next) {
         res.status(err.status || 500);
@@ -91,8 +103,7 @@ if (app.get('env') === 'development') {
     });
 }
 
-// production error handler
-// no stacktraces leaked to user
+// production error handler, no stacktraces leaked to user
 app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
@@ -100,6 +111,5 @@ app.use(function (err, req, res, next) {
         error: {}
     });
 });
-
 
 module.exports = app;
