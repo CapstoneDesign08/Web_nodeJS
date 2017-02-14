04a790457012fe93a4458eebbc43a07673deac69
hhhhm8826 <hhhhm8826@gmail.com>
2017-02-14 15:55:02 +0900

    1Cycle FIN (hong)

---
+++ Web_NodeJS/Web_NodeJS/upload/MyAssembly.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyAssembly : MonoBehaviour {
+
+	public static string myString="abc";
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Hello from start:"+ gameObject.name);
+		
+	}
+
+	public string LogMyString(){
+		return "Hello from DLL";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log ("Hello from update");
+	}
+}
